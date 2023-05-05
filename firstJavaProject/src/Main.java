92514f9d40a8083add63253a322ad06398a3096a
akanjibasirat <akanjibaseerah@gmail.com>
2023-05-05 23:38:35 +0100

    firstjava project

---
+++ firstJavaProject/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome to my initial commit!");
+
+        System.out.println("i use to think i do not have a favourite colour till i found out my closet is all blue");
+        System.out.println("i attended obafemi awolowo university ile ife");
+        System.out.println("becoming a soft ware engineer is now a must for me");
+        System.out.println("artificial intelligence rules the world ");
+
+
+        }
+    }
